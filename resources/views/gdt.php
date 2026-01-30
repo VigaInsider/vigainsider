647ebc4fdf008909b6b94942f5579e1853e4c45c
Mikekv2007 <githubeci@gmail.com>
2026-01-30 10:50:31 +0100

    Add 'Attività' and 'Orario' sections to GDT page for Earth Day event

--- resources/views/gdt.php
+++ resources/views/gdt.php
@@ -5,3 +5,26 @@
               'titolo' => 'Giornata della Terra'
        ]); 
        ?>
+
+
+       <!-- Attività -->
+       <?php component('cards/section-card', [
+                    'id' => 'attivitaGdt',
+                    'titolo' => 'Ecco le nostre Attività per la Giornata della Terra',
+                    'descrizione' => 'Scopri le iniziative e le attività speciali che abbiamo organizzato per celebrare la Giornata della Terra. Partecipa e fai la differenza!',
+                    'gradientFrom' => 'from-green-500',
+                    'gradientTo' => 'to-green-700',
+                    'iconPath' => 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z'
+                ]); 
+       ?>
+
+       <!-- Orario -->
+       <?php component('cards/section-card', [
+                    'id' => 'orarioGdt',
+                    'titolo' => 'Orario Speciale Giornata della Terra',
+                    'descrizione' => 'Consulta l\'orario speciale delle nostre attività in occasione della Giornata della Terra. Unisciti a noi per celebrare e proteggere il nostro pianeta!',
+                    'gradientFrom' => 'from-green-500',
+                    'gradientTo' => 'to-green-700',
+                    'iconPath' => 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z'
+                ]); 
+       ?>
