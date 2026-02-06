cbe647df072b04d90dc9151622b4604c22d40901
Vasco7274 <vscoppa365@gmail.com>
2026-02-06 16:03:18 +0100

    Add routes for VigaSec, creazioni, VigaNews, CICL, and Giochi di Informatica

--- public/index.php
+++ public/index.php
@@ -19,7 +19,36 @@ $router->get('/gdt', function() {
         'title' => 'VigaInsider - GdT',
     ]);
 });
-
+$router->get('/VGsolidare', function() {
+    view('VGsolidare', [
+        'title' => 'VigaInsider - Viga solidare',
+    ]);
+});
+$router->get('/VigaSec', function() {
+    view('VigaSec', [
+        'title' => 'VigaInsider - VigaSecurity',
+    ]);
+});
+$router->get('/creazioni', function() {
+    view('creazioni', [
+        'title' => 'VigaInsider - creazioni',
+    ]);
+});
+$router->get('/VigaNews', function() {
+    view('VigaNews', [
+        'title' => 'VigaInsider - VigaNews',
+    ]);
+});
+$router->get('/CICL', function() {
+    view('CICL', [
+        'title' => 'VigaInsider - CICLab',
+    ]);
+});
+$router->get('/GDI', function() {
+    view('GDI', [
+        'title' => 'VigaInsider - Giochi di Informatica',
+    ]);
+});
 $router->get('health', function() {
     echo json_encode(['status' => 'ok']);
 });
