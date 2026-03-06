a870fff8cf1256c929e5cd1ad81a3496f813532b
Orlando Rossi <linkhyruleanglassa2@gmail.com>
2026-03-06 20:29:56 +0100

    feat: Add 404 page with button component

--- resources/views/404.php
+++ resources/views/404.php
@@ -1,6 +1,16 @@
 <?php layout('components/layout'); ?>
 
-    <div class="container mx-auto px-4 py-16 max-w-7xl">
-        <p class="text-xlg">404</p>
-
+    <div class="container mx-5 my-3 inline text-center">
+        <h1 class="text-9xl">404</h1>
+        <p class="py-3">Purtroppo la pagina che cercavi non esiste (╥ ω ╥)</p>
+        
+        <div class="my-3">
+            <?php component("button", [
+            "content" => "Torna alla homepage",
+            "gradientFrom" => "from-blue-500",
+            "gradientTo" => "to-sky-400",
+            "text_color" => "text-[#F8F8F8]",
+            "destination" => "/"
+        ]) ?>
+        </div>
     </div>
