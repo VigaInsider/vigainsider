c36d28a4320e6899a0c2237c0ffa0063497d8309
milenaciccarese <ciccarese.milework@gmail.com>
2026-03-06 15:42:42 +0100

    Refactor code structure for improved readability and maintainability

--- resources/views/components/cards/section-card.php
+++ resources/views/components/cards/section-card.php
@@ -6,13 +6,12 @@
  * @param string $descrizione Descizione del contenuto della card (es., "Giornata della terra 2026.")
  * @param string $gradientFrom Classe Tailwind CSS per il colore di inizio del gradiente (es., "from-red-500")
  * @param string $gradientTo Classe Tailwind CSS per il colore di fine del gradiente (es., "to-red-700")
- * @param string $iconPath SVG path per lo sfondo della card (es., "M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zM11 17h2v-6h-2v6zm0-8h2V7h-2v2z")
+ * @param string $imgPath Percorso dell'immagine di sfondo della card (es., "card_giornalino.jpg")
  */ ?>
 <div id="<?= $id ?>" class="group rounded-xl overflow-hidden shadow-lg hover:shadow-2xl transition duration-300 transform hover:-translate-y-2">
     <div class="bg-gradient-to-br <?= $gradientFrom ?> <?= $gradientTo ?> h-40 flex items-center justify-center">
-        <svg class="w-24 h-24 text-white opacity-80" fill="currentColor" viewBox="0 0 24 24">
-            <path d="<?= $iconPath ?>"/>
-        </svg>
+        <img src="<?= $imgPath ?>" alt="<?= $titolo ?>" class="object-fill w-full h-full
+">
     </div>
     <div class="p-6 bg-white">
         <h3 class="text-2xl font-bold text-gray-800 mb-2"><?= $titolo ?></h3>
--- resources/views/home.php
+++ resources/views/home.php
@@ -14,7 +14,7 @@
                     'gradientFrom' => 'from-red-500',
                     'gradientTo' => 'to-red-700',
                     'route' => '/viganews',
-                    'iconPath' => 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z'
+                    'imgPath' => 'img/cards/card_giornalino.jpg'
                 ]); 
                 ?>
 
@@ -26,7 +26,7 @@
                     'gradientFrom' => 'from-purple-500',
                     'gradientTo' => 'to-purple-700',
                     'route' => '/creazioni',
-                    'iconPath' => 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z'
+                    'imgPath' => 'img/cards/card_creazioni.jpg'
                 ]); 
                 ?>
                 
@@ -38,7 +38,7 @@
                     'gradientFrom' => 'from-yellow-500',
                     'gradientTo' => 'to-yellow-700',
                     'route' => '/vigasecurity',
-                    'iconPath' => 'M12 1L3 5v6c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V5l-9-4zm0 10.99h7c-.53 4.12-3.28 7.72-7 8.77V12H5V6.3l7-3.11v8.8z'
+                    'imgPath' => 'img/cards/card_vigasecurity.jpg'
                 ]); 
                 ?>
 
@@ -50,7 +50,7 @@
                     'gradientFrom' => 'from-green-500',
                     'gradientTo' => 'to-green-700',
                     'route' => '/gdt',
-                    'iconPath' => 'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-4 15l-4-4 1.41-1.41L8 14.17l6.59-6.59L16 9l-8 8z'
+                    'imgPath' => 'img/cards/card_gdt.jpg'
                 ]); 
                 ?>
 
@@ -62,7 +62,7 @@
                     'gradientFrom' => 'from-pink-500',
                     'gradientTo' => 'to-pink-700',
                     'route' => '/vigasolidale',
-                    'iconPath' => 'M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z'
+                    'imgPath' => 'img/cards/campoarance.jpg'
                 ]); 
                 ?>
 
@@ -74,7 +74,7 @@
                     'gradientFrom' => 'from-indigo-500',
                     'gradientTo' => 'to-indigo-700',
                     'route' => '/giochi',
-                    'iconPath' => 'M21 6h-7V3c0-.55-.45-1-1-1h-4c-.55 0-1 .45-1 1v3H3c-.55 0-1 .45-1 1v14c0 .55.45 1 1 1h18c.55 0 1-.45 1-1V7c0-.55-.45-1-1-1zm-1 14H4V7h16v13zm-6-4c.83 0 1.5-.67 1.5-1.5S14.83 12 14 12s-1.5.67-1.5 1.5.67 1.5 1.5 1.5zm-4 0c.83 0 1.5-.67 1.5-1.5S10.83 12 10 12s-1.5.67-1.5 1.5.67 1.5 1.5 1.5z'
+                    'imgPath' => 'img/cards/card_giornalino.jpg'
                 ]); 
                 ?>
                 
@@ -86,7 +86,7 @@
                     'gradientFrom' => 'from-cyan-500',
                     'gradientTo' => 'to-cyan-700',
                     'route' => '/ciclab',
-                    'iconPath' => 'M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V5h14v14zm-5.04-6.71l-2.75 3.54 1.96 2.36h5.98L19 9z'
+                    'imgPath' => 'img/cards/card_giornalino.jpg'
                 ]); 
                 ?>
 
@@ -98,7 +98,7 @@
                     'gradientFrom' => 'from-orange-500',
                     'gradientTo' => 'to-orange-700',
                     'route' => '/vsw',
-                    'iconPath' => 'M12 4.81L15.09 8l1.41-1.41L12 .81l-4.5 4.5L9.91 8 12 4.81zM4.81 12L8 15.09l-1.41 1.41L.81 12l4.5-4.5L8 9.91 4.81 12zm14.38 0L16 15.09l1.41 1.41 4.5-4.5-4.5-4.5L16 9.91l2.19 2.09zM12 19.19l-3.09-3.09-1.41 1.41L12 23.19l4.5-4.5-1.41-1.41L12 19.19z'
+                    'imgPath' => 'img/cards/card_giornalino.jpg'
                 ]); 
                 ?>
 
